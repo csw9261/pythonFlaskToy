9ad0e04a5a6134fbe3a33e0c7bddd8f313248bae
SW CHOI <sangwon9261@naver.com>
2024-02-21 15:24:30 +0900

    1. Flask의 Default로 제공하는 Werkzeug의 developent WSGI Server에서 Gunicorn으로 변경 2. Gunicorn을 사용하기 위한 Dockerfile 추가 (Window에서는 Gunicorn을 사용할 수 없음)

--- app/app.py
+++ app/app.py
@@ -18,5 +18,8 @@ def page_server_error(error):
 app.register_blueprint(maincontroller.main_blueprint)
 app.register_blueprint(fileuploadcontroller.upload_blueprint)
     
-app.run(debug=True)
-#app.run()
+# Docker 컨테이너 내에서 Flask 애플리케이션을 Gunicorn과 함께 실행하려고 할 때, 
+# app.run()도 동시에 실행되려고 하면서 포트 충돌이 발생하기 때문에 해당 조건을 넣어야 한다.
+if __name__ == '__main__':
+    app.run(debug=True)
+
